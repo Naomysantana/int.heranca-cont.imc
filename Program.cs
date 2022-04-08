298e4bd6d5bb887a3d9b1f44073a0c9423dcbbea
Naomysantana <102303751+Naomysantana@users.noreply.github.com>
2022-04-08 15:54:58 -0300

    Cachorro

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Aul08VS;
+using System;
+
+namespace Aul08VS
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Cachorro Scooby;
+
+            Console.WriteLine("Hello word!");
+            Cachorro Scooby = new Cachorro("Scooby", "marrom", 7);
+
+            Scooby = new Cachorro();
+            Scooby.Nome = "Scooby";
+            Scooby.Cor = "marrom";
+            Scooby.Tamanho = 7;
+
+            Console.WriteLine($"Cachorro é o {Scooby.Nome} de cor {Scooby.Cor} e tem o tamanho {Scooby.Tamanho}");
+
+            Scooby.comer("De tudo");
+
+            Console.WriteLine(retorno);
+
+            
+        }
+    }
+}
